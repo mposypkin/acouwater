3416a5eba4848c65e6a49c925051706fdbff188a
Oleg Zaikin <zaikin.icc@gmail.com>
2017-02-10 20:01:31 +0800

    bug fix

--- ahw/testahw.cpp
+++ ahw/testahw.cpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <common/sgerrcheck.hpp>
 #include <common/utilmacro.hpp>
+#include <sspemdd_sequential.h>
 #include "acoustics_homog_water.hpp"
 
 int main(int argc, char *argv[])
@@ -12,9 +13,9 @@ int main(int argc, char *argv[])
 	sspemdd_sequential sspemdd_seq;
 	sspemdd_seq.readScenario("312_bottom_R_weighted260.txt");
 	std::vector<std::pair<double, double>> vPair;
-	vPair.push_back(std::make_pair(std::string(sspemdd_seq.R1, sspemdd_seq.R2)));
-	vPair.push_back(std::make_pair(std::string(sspemdd_seq.rhob1, sspemdd_seq.rhob2)));
-	vPair.push_back(std::make_pair(std::string(sspemdd_seq.cb1, sspemdd_seq.cb2)));
+	vPair.push_back(std::make_pair(sspemdd_seq.R1, sspemdd_seq.R2));
+	vPair.push_back(std::make_pair(sspemdd_seq.rhob1, sspemdd_seq.rhob2));
+	vPair.push_back(std::make_pair(sspemdd_seq.cb1, sspemdd_seq.cb2));
     ACOUSTIC::AcousticsHomogWaterProblemFactory ahwpf(vPair);
     COMPI::MPProblem<double>* prob = ahwpf.getProblem();
     double x[n] = {7019, 1.75, 1705};
